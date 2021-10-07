2f6f357fb690d6fbc4ee57f2391c139d965437d0
Ajit Rajurkar <ajit.rajurkar619@gmail.com>
2021-10-06 23:23:19 -0700

    spline

--- vai/assets/js/loader.js
+++ vai/assets/js/loader.js
@@ -2,9 +2,5 @@ $(window).load(function() {
 	$('#loader').addClass('is_active');
 	setTimeout(function() {
 		$('#loader').addClass('is_loaded');
-	}, 4000);
-	setTimeout(function() {
-		$('#loader_second').addClass('is_loaded');
-	}, 1800);
+	}, 15000);
 });
-
